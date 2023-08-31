31bcad03e42a50ea42ef06625430fe33a6936399
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-08-31 13:36:59 -0300

    página inicial update

--- conversor-moedas/src/assets/js/scripts.js
+++ conversor-moedas/src/assets/js/scripts.js
@@ -1,15 +1,6 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.6 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
+    // Ative o bootstrap scrollspy no elemento de navegação principal
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -18,7 +9,7 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
+    // Recolher a barra de navegação responsiva quando o alternador estiver visível
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
